a917970565b9c288df7a0a6f5e02e1e28cf46bc3
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-09-21 19:43:51 +0600

    member details through route

--- src/components/StaffDashboard/MemberDetailsModal.jsx
+++ src/components/StaffDashboard/MemberDetailsModal.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FaSearch, FaUser, FaEye } from 'react-icons/fa';
+import { useLocation, useNavigate } from 'react-router';
 
-const MemberDetailsModal = ({currentUser,setIsModalOpen}) => {
+const MemberDetailsModal = () => {
+    const {currentUser} = useLocation().state;
+    const navigate = useNavigate()
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-lg p-6 w-full max-w-md">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-semibold text-gray-900">Member Details</h2>
               <button
-                onClick={() => setIsModalOpen(false)}
+                onClick={() => navigate('/staff/members')}
                 className="text-gray-500 hover:text-gray-700 text-2xl"
               >
                 &times;
@@ -62,10 +65,10 @@ const MemberDetailsModal = ({currentUser,setIsModalOpen}) => {
             <div className="mt-6 flex justify-end">
               <button
                 type="button"
-                onClick={() => setIsModalOpen(false)}
+                onClick={() => navigate('/staff/members')}
                 className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               >
-                Close
+                Go Back
               </button>
             </div>
           </div>

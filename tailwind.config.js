ebf9ecee28ba5d12c39043b0a7ea3dc8234f556b
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-10-31 12:14:35 +0600

    forced and fixed dark mode [attempt 2]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-const daisyui = require("daisyui");
-
+// tailwind.config.js
 module.exports = {
-  plugins: [daisyui],
+  darkMode: "class", // not strictly needed, but recommended
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ['dark'],
+    themes: ["dark"], // or just ["dark"] if you never want light
+    darkTheme: "dark", // default dark theme
   },
 };

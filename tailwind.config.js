21ba0afdb717eed709c13e1d87f02ecc3b2f0451
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-10-31 12:06:59 +0600

    forced dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,6 @@ const daisyui = require("daisyui");
 module.exports = {
   plugins: [daisyui],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#2563eb",
-          secondary: "#64748b",
-          accent: "#22c55e",
-          neutral: "#1e293b",
-          "base-100": "#ffffff",
-          success: "#16a34a",
-          info: "#0ea5e9",
-          warning: "#facc15",
-          error: "#dc2626",
-        },
-      },
-    ],
+    themes: ['dark'],
   },
 };
